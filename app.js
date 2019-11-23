45e022dc843c4cc9d017703cd679701bc481c264
Ali Apple <toqeer.94@gmail.com>
2019-11-23 06:18:27 +0500

    Create Element and append to book list table

--- app.js
+++ app.js
@@ -12,7 +12,23 @@ function UI(){}
 
 // UI prototype
 UI.prototype.addBookToList = function(book){
-    console.log(book);
+    
+    const list = document.getElementById('book-list');
+
+    // Create Element
+    const row = document.createElement('tr'); 
+
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="delete">X</a></td>
+    
+    `;
+
+    list.appendChild(row);
+
+
 }
 
 
